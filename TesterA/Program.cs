f505975315388cab73f9122450f0f932e741df4a
Alexandry Cornita <alexandry.cornita.spo16@ecusolna.onmicrosoft.com>
2017-09-21 20:32:20 +0200

    Playing with int arrays and values to begin with

--- TesterA/Program.cs
+++ TesterA/Program.cs
@@ -10,6 +10,54 @@ namespace TesterA
 	{
 		static void Main(string[] args)
 		{
+			int[] arr = new int[5];
+
+			arr[0] = 1;
+			arr[1] = 5;
+			arr[2] = 6;
+			arr[3] = 0;
+			arr[4] = 10;
+
+			int min = arr.Min();
+			int max = arr.Max();
+			Console.WriteLine("min is " + min);
+			Console.WriteLine("mnax is " + max);
+
+			int[] arr2 = { 20, 55, 74, 32, 0 };
+			int min2 = arr2.Min();
+			int max2 = arr2.Max();
+			Console.WriteLine($"Min is {min2} and max is {max2}");
+
+			int[] arr3 = { -10, -5, 0, 4, 2, 1 };
+
+			//Gets the smallest number after 0
+			int minst = arr3.Where(n => n > 0).Min();
+			Console.WriteLine("Minsta ovan 0 är: " + minst);
+
+
+			//Gets the biggest number after 0
+			int störst = arr3.Where(n => n > 0).Max();
+			Console.WriteLine("Största ovan 0 är: " + störst);
+
+			
+			//Minsta under 10
+			const int lowest = 0;
+			foreach (int number in arr3)
+			{
+				if (number < lowest)
+					min = Math.Min(number, min);
+				Console.WriteLine($"Minsta under 0 är: {min}");
+			}
+
+			//Strörsta under 0
+			const int highest = 0;
+			foreach (int number in arr3)
+			{
+				if (number < highest)
+					max = Math.Max(number, min);
+				Console.WriteLine($"Minsta under 0 är: {max}");
+			}
+
 		}
 	}
 }
